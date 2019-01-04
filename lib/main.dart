99ff0654b80a07ebd27bc022b9d3c041205fd1ae
William Martins <owilliam.net@gmail.com>
2019-01-04 12:50:22 -0200

    Add package google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:firebase_setup/model/board.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'dart:async';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = GoogleSignIn();
 
 void main() => runApp(MyApp());
 
@@ -20,142 +23,58 @@ class MyApp extends StatelessWidget {
 }
 
 class MyNewHomePage extends StatefulWidget {
-  @override
   _MyNewHomePageState createState() => _MyNewHomePageState();
 }
 
 class _MyNewHomePageState extends State<MyNewHomePage> {
-  List<Board> boardMessages = List();
-  Board board;
-  final FirebaseDatabase database = FirebaseDatabase.instance;
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  DatabaseReference databaseReference;
-
-  @override
-  void initState() {
-    super.initState();
-    board = Board("", "");
-    databaseReference = database.reference().child("community_board");
-    databaseReference.onChildAdded.listen(_onEntryAdded);
-    databaseReference.onChildChanged.listen(_onEntryChanged);
-  }
-
-  /*   void _incrementCounter() {
-            database.reference().child("message").set({
-              "firstname": "William",
-              "lastname": "Martins",
-              "Age": 42,
-            });
-        
-            setState(() {
-              //This is our callback
-              database
-                  .reference()
-                  .child("message")
-                  .once()
-                  .then((DataSnapshot snapshot) {
-                //Map<dynamic, dynamic> dataValues = snapshot.value;
-        
-                print("Values from db: ${snapshot.value.toString().toUpperCase()}");
-              });
-        
-              _counter++;
-            });
-          } 
-        */
+  String _imageURL;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Community Board"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Flexible(
-              flex: 0,
-              child: Center(
-                child: Form(
-                  key: formKey,
-                  child: Flex(
-                    direction: Axis.vertical,
-                    children: <Widget>[
-                      ListTile(
-                        leading: Icon(Icons.subject),
-                        title: TextFormField(
-                          initialValue: "",
-                          onSaved: (val) => board.subject = val,
-                          validator: (val) => val == "" ? val : null,
-                        ),
-                      ),
-
-                      ListTile(
-                        leading: Icon(Icons.message),
-                        title: TextFormField(
-                          initialValue: "",
-                          onSaved: (val) => board.body = val,
-                          validator: (val) => val == "" ? val : null,
-                        ),
-                      ),
-
-                      //Send Post
-                      FlatButton(
-                        child: Text("Post"),
-                        color: Colors.blueAccent,
-                        onPressed: () {
-                          handleSubmit();
-                        },
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Flexible(
-              child: FirebaseAnimatedList(
-                query: databaseReference,
-                itemBuilder: (_, DataSnapshot snapshot,
-                    Animation<double> animation, int index) {
-                  return Card(
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.grey,
-                      ),
-                      title: Text(boardMessages[index].subject),
-                      subtitle: Text(boardMessages[index].body),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ));
+      appBar: AppBar(
+        title: Text('Board'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          FlatButton(
+            child: Text('Google Sign-in'),
+            color: Colors.blue[400],
+            onPressed: () => _gSignin(),
+          ),
+          FlatButton(
+            child: Text('Sign-in with E-mail'),
+            color: Colors.orange[400],
+            onPressed: () {},
+          ),
+          FlatButton(
+            child: Text('Create account'),
+            color: Colors.teal[400],
+            onPressed: () {},
+          ),
+          Image.network(_imageURL == null || _imageURL.isEmpty
+              ? "https://via.placeholder.com/150"
+              : _imageURL),
+        ],
+      ),
+    );
   }
 
-  void _onEntryAdded(Event event) {
-    setState(() {
-      boardMessages.add(Board.fromSnapshot(event.snapshot));
-    });
-  }
+  Future<FirebaseUser> _gSignin() async {
+    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
 
-  void handleSubmit() {
-    final FormState form = formKey.currentState;
-    if (form.validate()) {
-      form.save();
-      form.reset();
-      //save form data to the database
-      databaseReference.push().set(board.toJson());
-    }
-  }
+    FirebaseUser user = await _auth.signInWithGoogle(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
 
-  void _onEntryChanged(Event event) {
-    var oldEntry = boardMessages.singleWhere((entry) {
-      return entry.key == event.snapshot.key;
-    });
+    print("User is: ${user.displayName}");
 
     setState(() {
-      boardMessages[boardMessages.indexOf(oldEntry)] =
-          Board.fromSnapshot(event.snapshot);
+      _imageURL = user.photoUrl;
     });
   }
 }
